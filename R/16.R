9367485013aeb6a002646f78e185b41fe8b08b11
模型机器 <aulihong@qq.com>
2025-11-24 11:34:46 +0800

    Add files via upload

---
+++ R/16.R
@@ -0,0 +1,24 @@
+#' @export
+mxjq_ulogit <- function(data, xat, yat){
+  xnamelevel <- mxjq_onn(data[xat])
+  ulogit <- list()
+  for (i in xat) {
+    datai <- data[, c(i, yat)]
+    colnames(datai)[2] <- c("Y")
+    glmi <- glm(Y ~ ., data = datai, family = binomial)
+    res2 <- broom::tidy(glmi, exponentiate = T, conf.int = T) %>%
+      mutate(
+        xname = colnames(data)[i],
+        uOR = round(estimate, 2),
+        uCI = paste0(round(conf.low, 3), "-", round(conf.high, 3)),
+        upvalue = p.value
+      ) %>%
+      select(xname, term, uOR, uCI, upvalue)
+    ulogit[[i]] <- res2
+  }
+  ulogit2 <- bind_rows(ulogit)
+  ulogit3 <- xnamelevel %>%
+    left_join(ulogit2, by = c("X"="xname", "Term"="term")) %>%
+    mutate_all(function(x)ifelse(is.na(x), "", x))
+  return(ulogit3)
+}
