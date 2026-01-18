2f287f984b1fa3209175c07d5ab0ee874f1f44cb
模型机器 <aulihong@qq.com>
2026-01-18 10:41:08 +0800

    Delete R directory

--- R/6.R
+++
@@ -1,9 +0,0 @@
-#' @export
-mxjq_ndn <- function (var, lvl, ordinal = FALSE) {
-  args <- vctrs::vec_recycle_common(var, lvl)
-  var <- args[[1]]
-  # coder wechat AuTrader
-  lvl <- args[[2]]
-  nms <- paste(var, lvl, sep = "_")
-  nms
-}
--- R/9.R
+++
@@ -1,6 +0,0 @@
-#' @export
-mxjq_r01 <- function(x){
-  r01 <- (x - min(x))/(max(x) - min(x))
-  # coder wechat AuTrader
-  return(r01)
-}
