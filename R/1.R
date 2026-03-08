65c6358f67813eeb812b57fa7ab752df902ba415
模型机器 <aulihong@qq.com>
2026-03-08 15:24:34 +0800

    Delete R directory

--- R/1.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  message("欢迎使用 ", pkgname)
-  message("如遇问题请联系我的微信AuTrader")
-}
--- R/mxjq_ia.R
+++
@@ -1,9 +0,0 @@
-#' @export
-mxjq_ia <- function(x, y) {
-  nobs <- length(x)
-  # coder wechat AuTrader
-  dx <- x[-1] - x[-nobs]
-  dy <- (y[-nobs] + y[-1])/2
-  ia <- sum(dx * dy)
-  return(ia)
-}
